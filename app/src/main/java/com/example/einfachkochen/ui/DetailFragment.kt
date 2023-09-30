ef6d125880a86324f37055d5eb01272dadef3795
Andreas Pelczer <134264285+AndreasPelczer@users.noreply.github.com>
2023-09-30 18:10:03 +0200

    3rv,tv,favorit.

--- app/src/main/java/com/example/einfachkochen/ui/DetailFragment.kt
+++ app/src/main/java/com/example/einfachkochen/ui/DetailFragment.kt
@@ -20,7 +20,7 @@ class DetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
